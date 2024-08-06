ef2548921e6ff71769ab9717dd14a55639c9dc2b
Daniel Maulding <danielmaulding@gmail.com>
2024-08-06 16:47:09 -0500

    Refactor variable names and update import statements
    Renamed various variables and updated import statements for clarity. Adjusted exception handling to use more descriptive variable names. This improves code readability and maintainability.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, NoSuchElementException, \
     WebDriverException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
 import pandas as pd
 import time
 from geopy.geocoders import Nominatim
@@ -13,15 +16,14 @@ import pickle
 import threading
 from ttkthemes import ThemedStyle
 from tkinter import ttk
-
-
-
+from geopy.exc import GeocoderServiceError
 
 # Define global variables for the UI Entry widgets
 excelFilePath = ""
 entry_location = None
 entry_keyword = None
 status_label = None
+
 
 def get_lat_long(location):
     try:
@@ -33,19 +35,16 @@ def get_lat_long(location):
             print("Location not found")
             set_status_message("Could not find City or Zip")
             return None, None
-    except geopy.exc.GeocoderServiceError as e:
-        print(f"Geocoder service error: {e}")
+    except geopy.exc.GeocoderServiceError as geo_service_error:
+        print(f"Geocoder service error: {geo_service_error}")
         return None, None
-    except Exception as e:
-        print(f"Error during geocoding: {e}")
+    except Exception as exception_e:
+        print(f"Error during geocoding: {exception_e}")
         return None, None
 
-#Number of times you want the program to scroll through pages
+
+# Number of times you want the program to scroll through pages
 Search_Attempts = 50
-
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 # Maximum time to wait (in seconds)
 MAX_WAIT = 10
@@ -63,6 +62,7 @@ if is_valid_address_format(address):
     print(f"'{address}' is in a valid format.")
 else:
     print(f"'{address}' is not in a valid format.")
+
 
 def get_businesses(location, search_keyword, existing_data):
     latitude, longitude = get_lat_long(location)
@@ -91,7 +91,7 @@ def get_businesses(location, search_keyword, existing_data):
     unique_businesses = set()
     scroll_attempts = 0
     max_scroll_attempts = Search_Attempts  # Maximum number of scrolled businesses searches. 10 = 100 businesses
-    Time_delay = 3  # Time delay for content loading
+    time_delay = 3  # Time delay for content loading
     scroll_height = 2500  # Scroll height
     last_processed_index = -1  # Last processed business index
 
@@ -100,14 +100,13 @@ def get_businesses(location, search_keyword, existing_data):
         driver.execute_script(f"window.scrollBy(0, {scroll_height});")
         wait = WebDriverWait(driver, MAX_WAIT)
 
-
         try:
-            business_links = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a.hfpxzc")))
+            business_links = wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, "a.hfpxzc")))
             print(f"Number of business links found: {len(business_links)}")
-
 
             # Start processing from the last processed index + 1
             for i in range(last_processed_index + 1, len(business_links)):
+                name = None
                 try:
                     business = business_links[i]
                     name = business.get_attribute('aria-label').strip()
@@ -116,13 +115,13 @@ def get_businesses(location, search_keyword, existing_data):
 
                     # Scroll to and click on each business to load its details
                     driver.execute_script("arguments[0].scrollIntoView(true);", business)
-                    time.sleep(Time_delay)
+                    time.sleep(time_delay)
                     driver.execute_script("arguments[0].click();", business)
-                    time.sleep(Time_delay)
+                    time.sleep(time_delay)
 
                     # Extract the business address
                     address_element = wait.until(
-                        EC.visibility_of_element_located((By.CSS_SELECTOR, "div.Io6YTe.fontBodyMedium.kR99db")))
+                        ec.visibility_of_element_located((By.CSS_SELECTOR, "div.Io6YTe.fontBodyMedium.kR99db")))
                     address = address_element.text
 
                     if (name, address) in existing_data or (name, address) in unique_businesses:
@@ -142,7 +141,7 @@ def get_businesses(location, search_keyword, existing_data):
 
                 except StaleElementReferenceException:
                     print(f"Stale element encountered for business {name}, retrying...")
-                    business_links = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a.hfpxzc")))
+                    business_links = wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, "a.hfpxzc")))
                     continue
                 except TimeoutException:
                     print(f"Timeout encountered while fetching address for {name}")
@@ -212,13 +211,12 @@ def save_to_excel(business_list, file_path="Businesses.xlsx"):
         except FileNotFoundError:
             updated_df = new_data_df
 
-        #print(f"Data to be written to Excel: \n{updated_df}")  # Add this
+        # print(f"Data to be written to Excel: \n{updated_df}")  # Add this
         updated_df.to_excel(file_path, index=False)
         print("Data has been written to Excel.")  # And this
 
     except Exception as e:
         print("Error occurred in save_to_excel: ", repr(e))
-
 
 
 def set_status_message(message):
@@ -240,9 +238,9 @@ def run_script_thread():
     try:
         # Get user input
         search_keyword = entry_keyword.get()
-        City = entry_location.get()
+        city = entry_location.get()
 
-        latitude, longitude = get_lat_long(City)
+        latitude, longitude = get_lat_long(city)
         if latitude is None or longitude is None:
             print("Stopping the execution as Latitude or Longitude couldn't be obtained")
             return
@@ -253,7 +251,7 @@ def run_script_thread():
         initial_unique_count = len(existing_data)
 
         # Get businesses
-        businesses, _, _ = get_businesses(City, search_keyword, existing_data)
+        businesses, _, _ = get_businesses(city, search_keyword, existing_data)
         print(f"Businesses: {businesses}")  # added prints for debugging
 
         # Save, update data and print results
@@ -261,7 +259,7 @@ def run_script_thread():
             # Save locations, keywords, and file paths to a pickle file
             with open('previous_inputs.pkl', 'wb') as f:
                 pickle.dump({
-                    'location': City,
+                    'location': city,
                     'keyword': search_keyword,
                     'excel_file_path': excelFilePath,
                 }, f)
@@ -284,6 +282,7 @@ def run_script_thread():
         # Re-enable the button when the script is done
         def reenable_button():
             run_button.config(state="normal")
+
         root.after(100, reenable_button)
 
 
@@ -297,6 +296,8 @@ if __name__ == "__main__":
         root.iconbitmap(r'C:\Users\danie\PycharmProjects\pythonProject\ICON.ico')
     except Exception as e:
         print(f"Error setting icon: {e}")
+
+    style = None
 
     try:
         root.configure(background='#353535')
@@ -335,8 +336,6 @@ if __name__ == "__main__":
     entry_location['state'] = 'normal'  # or 'readonly', 'disabled'
     entry_location.focus_set()
 
-
-
     status_label = ttk.Label(root, text="", style='Custom.TLabel')
     status_label.grid(row=4, column=0, sticky='ew')
 
@@ -345,14 +344,15 @@ if __name__ == "__main__":
     try:
         with open('previous_inputs.pkl', 'rb') as f:
             previous_inputs = pickle.load(f)
-    except:
+    except (FileNotFoundError, pickle.UnpicklingError):
         pass
 
     entry_keyword.insert(0, previous_inputs['keyword'])
     entry_location.insert(0, previous_inputs['location'])
     excelFilePath = previous_inputs['excel_file_path']
 
-    def Set_excel_file():
+
+    def set_excel_file():
         global excelFilePath
         temp_file_path = tk.filedialog.askopenfilename(initialdir="/", title="Select file",
                                                        filetypes=(("Excel files", "*.xlsx"), ("all files", "*.*")))
@@ -360,14 +360,11 @@ if __name__ == "__main__":
             excelFilePath = temp_file_path
         print(f"Chosen file path: {excelFilePath}")
 
-    Set_button = ttk.Button(root, text="Set Excel File Path", command=Set_excel_file, style='Custom.TButton')
+
+    Set_button = ttk.Button(root, text="Set Excel File Path", command=set_excel_file, style='Custom.TButton')
     Set_button.grid(row=5, column=0, padx=10, pady=0, sticky='ew')
-
-
-
 
     run_button = ttk.Button(root, text="Search", command=run_script, style='Custom.TButton')
     run_button.grid(row=6, column=0, padx=10, pady=10, sticky='ew')
 
     root.mainloop()
-
